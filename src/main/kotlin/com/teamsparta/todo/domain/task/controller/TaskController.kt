ec54ade7dea2cb7bb22a1549b0f16d6ecf052a71
DanDanjoo <murekal94@gmail.com>
2024-05-23 16:40:48 +0900

    TaskController HTTP요청 추가 및 주입 받은 service 코드 추가

--- src/main/kotlin/com/teamsparta/todo/domain/task/controller/TaskController.kt
+++ src/main/kotlin/com/teamsparta/todo/domain/task/controller/TaskController.kt
@@ -7,6 +7,7 @@ import com.teamsparta.todo.domain.task.dto.UpdateTaskRequest
 import com.teamsparta.todo.domain.task.service.TaskService
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
@@ -23,31 +24,42 @@ class TaskController(
     @PostMapping
     @Operation(summary = "Task 만들기", description = "새로운 Task 항목을 생성합니다.")
     fun createdTask(@RequestBody createTaskRequest: CreateTaskRequest): ResponseEntity<TaskResponse> {
-        TODO()
+        return ResponseEntity
+            .status(HttpStatus.CREATED)
+            .body(taskService.createTask(createTaskRequest))
     }
 
     @GetMapping
     @Operation(summary = "Task 모두 조회하기", description = "모든 Task 항목을 조회합니다.")
     fun getAlltaskList(): ResponseEntity<List<TaskResponse>> {
-        TODO()
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(taskService.getAllTaskList())
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/{taskId}")
     @Operation(summary = "Task Id로 조회하기", description = "Id로 Task 항목을 조회합니다.")
-    fun getTaskById(@PathVariable id: Long): ResponseEntity<TaskResponse> {
-        TODO()
+    fun getTaskById(@PathVariable taskId: Long): ResponseEntity<TaskResponse> {
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(taskService.getTaskById(taskId))
     }
-    @PutMapping("/{id}")
+    @PutMapping("/{taskId}")
     @Operation(summary = "Task Id로 수정하기", description = "Id로 Task 항목을 수정합니다.")
     fun updateTask(
-        @PathVariable id: Long,
+        @PathVariable taskId: Long,
         @RequestBody updateTaskRequest: UpdateTaskRequest
     ): ResponseEntity<TaskResponse> {
-        TODO()
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(taskService.updateTask(taskId, updateTaskRequest))
     }
-    @DeleteMapping("/{id}")
+    @DeleteMapping("/{taskId}")
     @Operation(summary = "Task Id로 삭제하기", description = "Id로 Task 항목을 삭제합니다.")
-    fun deleteTask(@PathVariable id: Long): ResponseEntity<Unit> {
-        TODO()
+    fun deleteTask(@PathVariable taskId: Long): ResponseEntity<Unit> {
+        taskService.deleteTask(taskId)
+        return ResponseEntity
+            .status(HttpStatus.NO_CONTENT)
+            .build()
     }
 }
--- src/main/kotlin/com/teamsparta/todo/domain/task/service/TaskService.kt
+++ src/main/kotlin/com/teamsparta/todo/domain/task/service/TaskService.kt
@@ -16,5 +16,5 @@ interface TaskService {
 
     fun updateTask(taskId: Long, request: UpdateTaskRequest): TaskResponse
 
-    fun deleteTaskById(taskId: Long): Task
+    fun deleteTask(taskId: Long): Task
 }
