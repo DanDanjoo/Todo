2d773bf09e704bfefa59c16cd3054312d79dcf30
DanDanjoo <murekal94@gmail.com>
2024-05-23 14:26:07 +0900

    도메인 패키지 생성 및 swagger 설정

---
+++ src/main/kotlin/com/teamsparta/todo/infra/swagger/SwaggerConfig.kt
@@ -0,0 +1,21 @@
+package com.teamsparta.todo.infra.swagger
+
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.info.Info
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+
+@Configuration
+class SwaggerConfig {
+
+
+    @Bean
+    fun openAPI(): OpenAPI = OpenAPI()
+        .info(
+             Info()
+                 .title("Todo API")
+                 .description("This is an example API")
+                 .version("2.0.0")
+        )
+}
